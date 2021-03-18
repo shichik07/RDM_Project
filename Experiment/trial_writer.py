ec77403042a5937889af8d8c662771b5463585e0
Julius kricheldorff <julius@kricheldorff.de>
2021-03-18 15:50:22 +0100

    simple class to write trial information in a csv file

---
+++ Experiment/trial_writer.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 12:51:38 2021
+
+@author: jules
+"""
+import csv, os
+
+class task_writer():
+    def __init__(self, loc_string):
+        self.trial_dict = {'Trial_nr': None , 
+              'Condition':None, 
+              'Gender': None,
+              'Correct': None,
+              'Coherence': None,
+              'Response': None,
+              'Group': None,
+              'Direction': None,
+              'Age': None,
+              'Block': None,
+              'RT': None,
+              'Colors': None,
+              'ISI': None,
+              'Early_resp': None}
+        self.loc = loc_string
+
+    
+    def set_file(self):
+        # Check Saving Location Directory
+        data_dir = os.path.abspath(self.loc + '/data/') # just in case the absolute path is required
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+    
+        #create a text file to save data
+        fileName = data_dir + '/RDM_PD' + 'participantX' + '_today' + '.csv'
+        cnt = 0
+
+        #Make sure we do not overwrite our data
+        while os.path.exists(fileName):
+            cnt += 1
+            fileName = data_dir + '/RDM_PD' + 'participantX' + '_today' + '_v' + str(cnt) + '.csv'
+
+        # Get the Fieldnames
+        fnames = list(self.trial_dict.keys()) 
+        # write a file
+        self.dataFile = open(fileName, 'w')  # a simple text file with 'comma-separated-values'
+        self.writer = csv.DictWriter(self.dataFile, fieldnames = fnames)
+        self.writer.writeheader()
+        
+    def finish(self):
+        self.dataFile.close()
+    
+    def update(self, trl_info):
+        for item in trl_info.items():
+            self.trial_dict[item[0]] = item[1]
+        self.writer.writerow(self.trial_dict)
+        
+
+#%% Test
+    
+save_path =  '/home/jules/Dropbox/PhD_Thesis/DecisionMakingAndLearningStudy/Experiment/Development' 
+wrt = task_writer(save_path)
+# Initialize file
+wrt.set_file()
+
+# Update file
+new_dict = {'Condition': 'AB', 'Age': 23}
+wrt.update(new_dict)
+wrt.finish()
+
