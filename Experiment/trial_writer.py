d76ac2b4ab61c1e4cd0d65aecca79fcae34c9fee
Julius kricheldorff <julius@kricheldorff.de>
2021-03-19 13:49:20 +0100

    added a function to reopen the file so intermediate results between blocks can be saved

--- Experiment/trial_writer.py
+++ Experiment/trial_writer.py
@@ -33,23 +33,28 @@ class task_writer():
             os.makedirs(data_dir)
     
         #create a text file to save data
-        fileName = data_dir + '/RDM_PD' + 'participantX' + '_today' + '.csv'
+        self.fileName = data_dir + '/RDM_PD' + 'participantX' + '_today' + '.csv'
         cnt = 0
 
         #Make sure we do not overwrite our data
-        while os.path.exists(fileName):
+        while os.path.exists(self.fileName):
             cnt += 1
-            fileName = data_dir + '/RDM_PD' + 'participantX' + '_today' + '_v' + str(cnt) + '.csv'
+            self.fileName = data_dir + '/RDM_PD' + 'participantX' + '_today' + '_v' + str(cnt) + '.csv'
 
         # Get the Fieldnames
-        fnames = list(self.trial_dict.keys()) 
+        self.fnames = list(self.trial_dict.keys()) 
         # write a file
-        self.dataFile = open(fileName, 'w')  # a simple text file with 'comma-separated-values'
-        self.writer = csv.DictWriter(self.dataFile, fieldnames = fnames)
+        self.dataFile = open(self.fileName, 'w')  # a simple text file with 'comma-separated-values'
+        self.writer = csv.DictWriter(self.dataFile, fieldnames = self.fnames)
         self.writer.writeheader()
         
     def finish(self):
         self.dataFile.close()
+    
+    def start(self):
+        self.dataFile = open(self.fileName, 'a') 
+        self.writer = csv.DictWriter(self.dataFile,  fieldnames = self.fnames)
+
     
     def update(self, trl_info):
         for item in trl_info.items():
@@ -65,7 +70,9 @@ wrt = task_writer(save_path)
 wrt.set_file()
 
 # Update file
-new_dict = {'Condition': 'AB', 'Age': 23}
+new_dict = {'Condition': 'AB', 'Age':22}
 wrt.update(new_dict)
 wrt.finish()
-
+wrt.start()
+wrt.update(new_dict)
+wrt.finish()
