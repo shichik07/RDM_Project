3f4eddecc5cde47d964c90a26e92bdb07d0b172d
Julius kricheldorff <julius@kricheldorff.de>
2021-03-26 14:33:57 +0100

    item struct now provides a basic item list for our experiment. Note all is hard-coded and you should stay away from it as far as you can

--- Experiment/item_struct.py
+++ Experiment/item_struct.py
@@ -13,12 +13,14 @@ import numpy as np
 class GetBlockList(object):
     def __init__(self):
         # Define List parameter
+        self.BlockBias = [1,3,5], [0,2,4]
         self.Trial_Nr = 36
         self.Coherence_Levels = [0, 0.1, 0.2, 0.5]
         self.Conditions = ['Mono', 'Di_null', 'Di_part', 'Di_full']
         self.Directions = ['left', 'right']
+        self.minorDir = ['left', 'right']
         self.Block = 6
-        self.Color = [[ -1,0,1],[ 1,0,1]], [[ -1,0,1],[ 1,0,1]], [[ -1,0,1],[ 1,0,1]], [[ -1,0,1],[ 1,0,1]]
+        self.Color = [[ -1,0,1],[ 1,0,1]], [[ -1,0,1],[ 1,0,1]], [[ 7,7,7],[ 1,1,1]], [[ 9,9,9],[ 2,2,2]]
         self.Color = pd.Series(self.Color)
         self.proportion = [0.2, 1.8]
         self.Trial_total = self.Trial_Nr* len(self.Conditions)*len(self.Coherence_Levels)
@@ -29,10 +31,23 @@ class GetBlockList(object):
                                        'Coherence': [None]*self.Trial_total,
                                     'Colors': [None]*self.Trial_total,
                                          'Direction': [None]*self.Trial_total,
-                                         'Block': [None]*self.Trial_total},
-                                  'Coherence_total': [None]*self.Trial_total)
+                                         'Block': [None]*self.Trial_total,
+                                  'Coherence_total': [None]*self.Trial_total})
         
-    def init_list(self, colors):
+    def init_list(self, colors, Block_order):
+        # determine in which manner we bias our list allocation (to reach the 75|25 color imbalance)
+        if Block_order == 1:
+            bias_con = self.BlockBias[1]
+        elif Block_order == 0:
+             bias_con = self.BlockBias[0]
+        else: 
+            raise ValueError('Please specify Block_order as either 0 or 1') 
+            
+        #set color
+        self.Color = colors
+            
+        # important to shuffle coherence values
+        random.shuffle(self.Coherence_Levels)
         ind = 0
         for blck in range(self.Block):    
             for con_idx, con in enumerate(self.Conditions):
@@ -43,30 +58,48 @@ class GetBlockList(object):
                             self.Items.loc[ind, 'Block'] = blck
                             self.Items.loc[ind, 'Condition'] = con
                             self.Items.at[ind, 'Coherence'] = self.translate_coherence(con, coh)
-                            self.Items.loc[ind, 'Coherence_total'] coh
+                            self.Items.loc[ind, 'Coherence_total'] = coh
                             self.Items.loc[ind, 'Direction'] = self.Directions[ind%len(self.Directions)]
                             self.Items.at[ind, 'Colors'] = self.Color[con_idx]
-                            
+                          
                             if con == self.Conditions[3]:
-                                if coh_count <= 7:
+                                if coh_count <= 3:
                                     #Swap the colors
                                     self.Items.at[ind, 'Colors'] = self.reverse_color(self.Items.at[ind, 'Colors'])
-                                if coh_count == 7 or coh_count == 7:
-                                    #determine direction at random (we have 6 reps per unique item - 75% of those need to be for one color)
-                                    # in order to have items approximately balanced across blocks one item needs to be selected at random determined
-                                      self.Items.loc[ind, 'Direction'] = random.choice(self.Directions)
-                            if con == self.Conditions[2]:
-                                if coh_count <= 5:
+                                # if coh_count == 7 or coh_count == 7:
+                                #     #determine direction at random (we have 6 reps per unique item - 75% of those need to be for one color)
+                                #     # in order to have items approximately balanced across blocks one item needs to be selected at random determined
+                                #       self.Items.loc[ind, 'Direction'] = random.choice(self.Directions)
+                            elif con == self.Conditions[2]:
+                                if coh_count <= 3:
                                     #Swap Colors
                                     self.Items.at[ind, 'Colors'] = self.reverse_color(self.Items.at[ind, 'Colors'])
+                                    
+                            # To make the last two directions random - otherwise we will have the low probability relevant condition more often left than right moving
+                            if con == self.Conditions[2] or con == self.Conditions[3]:
+                                if coh_count == 4:
+                                    random.shuffle(self.minorDir)
+                                    self.Items.loc[ind, 'Direction'] = self.minorDir[0]
+                                elif coh_count == 5:
+                                    #here take the other direction helps us to get a more unbiased list for each participant
+                                    self.Items.loc[ind, 'Direction'] = self.minorDir[1]
+                            
+                            # Here we make sure that overall we get to our 75%|25% major color distinction per majority coherence
+                            # in an approximately random manner
+                            if blck in bias_con and coh_count == 4:
+                                if coh == self.Coherence_Levels[1] or coh == self.Coherence_Levels[3]:
+                                    self.Items.at[ind, 'Colors'] = self.reverse_color(self.Items.at[ind, 'Colors'])
+                            elif blck not in bias_con and coh_count == 4:
+                                if coh == self.Coherence_Levels[0] or coh == self.Coherence_Levels[2]:
+                                    self.Items.at[ind, 'Colors'] = self.reverse_color(self.Items.at[ind, 'Colors'])
+                                
                             # Update indices
                             ind +=1
                             coh_count += 1
-        return self.Items 
+        return self.Items
     
     def reverse_color(self,entry):
-        col = entry
-        list.reverse(col)
+        col = entry[::-1]
         return col
     
     def translate_coherence(self, condition, coherence):
@@ -78,8 +111,22 @@ class GetBlockList(object):
             coh_two = [[round(coherence*self.proportion[0],2), round(coherence*self.proportion[1],2)]]
         return coh_two
     
-#%% Playground
-
-           
+#%% How to use
 a = GetBlockList()
-lis = a. init_list(3)
+BiasC =  [0,1]
+colors = [[ -1,0,1],[ 1,0,1]], [[ 3,3,3],[ 5,5,5]], [[ 7,7,7],[ 1,1,1]], [[ 9,9,9],[ 2,2,2]]
+colors2 = [None]*4
+for ix,col in enumerate(colors): 
+    color2[ix] = a.reverse_color(col)
+
+new_color_ord
+# for x in y:
+    # bs = BiasC[x%2]
+    # if x%4 <=2:
+    #     col = colors
+    # else:
+    #     col = colors2
+    # lis= a. init_list(3, bs)
+
+lis= a. init_list(3, BiasC[0])
+
